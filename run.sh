52b7f30fbc629c9be1ecd69c593540b5abe80337
Joshua Trzebiatowski <joshua.trzebiatowski@kohls.com>
2022-05-09 16:49:35 -0500

    export VAULT_ADDR

--- run.sh
+++ run.sh
@@ -11,20 +11,20 @@ chmod +x vault jq
 
 echo "$VCAP_SERVICES"
 ls -la
-VAULT_ADDR="$(echo "$VCAP_SERVICES" | ./jq '."hashicorp-vault"[0].credentials.address' | tr -d '"')"
+export VAULT_ADDR="$(echo "$VCAP_SERVICES" | ./jq '."hashicorp-vault"[0].credentials.address' | tr -d '"')"
 VAULT_TOKEN="$(echo "$VCAP_SERVICES" | ./jq '."hashicorp-vault"[0].credentials.auth.token' | tr -d '"')"
 SERVICE_ENGINE="$(echo "$VCAP_SERVICES" | ./jq '."hashicorp-vault"[0].credentials.backends.generic[0]' | tr -d '"')"
 APPLICATION_ENGINE="$(echo "$VCAP_SERVICES" | ./jq '."hashicorp-vault"[0].credentials.backends_shared.application' | tr -d '"')"
 SPACE_ENGINE="$(echo "$VCAP_SERVICES" | ./jq '."hashicorp-vault"[0].credentials.backends_shared.space' | tr -d '"')"
 ORGANIZATION_ENGINE="$(echo "$VCAP_SERVICES" | ./jq '."hashicorp-vault"[0].credentials.backends_shared.organization' | tr -d '"')"
-./vault login -address="$VAULT_ADDR" "$VAULT_TOKEN"
+./vault login "$VAULT_TOKEN"
 
 while true; do
   ./vault token renew
-  echo "SERVICE:      $(./vault read -address="$VAULT_ADDR" "${SERVICE_ENGINE}/secret" -field=SECRET_TYPE)"
-  echo "APPLICATION:  $(./vault read -address="$VAULT_ADDR" "${APPLICATION_ENGINE}/secret" -field=SECRET_TYPE)"
-  echo "SPACE:        $(./vault read -address="$VAULT_ADDR" "${SPACE_ENGINE}/secret" -field=SECRET_TYPE)"
-  echo "ORGANIZATION: $(./vault read -address="$VAULT_ADDR" "${ORGANIZATION_ENGINE}/secret" -field=SECRET_TYPE)"
+  echo "SERVICE:      $(./vault read "${SERVICE_ENGINE}/secret" -field=SECRET_TYPE)"
+  echo "APPLICATION:  $(./vault read "${APPLICATION_ENGINE}/secret" -field=SECRET_TYPE)"
+  echo "SPACE:        $(./vault read "${SPACE_ENGINE}/secret" -field=SECRET_TYPE)"
+  echo "ORGANIZATION: $(./vault read "${ORGANIZATION_ENGINE}/secret" -field=SECRET_TYPE)"
   echo ''
   sleep 10
 done
