a8e3cf32ba32d3a2867b17409af3635f29101024
Joshua Trzebiatowski <joshua.trzebiatowski@kohls.com>
2022-05-09 16:38:22 -0500

    add jq binary

--- run.sh
+++ run.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 VAULT_BIN_URL="https://releases.hashicorp.com/vault/1.10.2/vault_1.10.2_linux_amd64.zip"
-JQ_BIN_URL="https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64"
 
 set -x
-wget -O jq "$JQ_BIN_URL"
 wget -O vault.zip "$VAULT_BIN_URL"
 unzip vault.zip
 
+echo "$VCAP_SERVICES"
 VAULT_ADDR="$(echo "$VCAP_SERVICES" | ./jq '."hashicorp-vault"[0].credentials.address')"
 VAULT_TOKEN="$(echo "$VCAP_SERVICES" | ./jq '."hashicorp-vault"[0].credentials.auth.token')"
 SERVICE_ENGINE="$(echo "$VCAP_SERVICES" | ./jq '."hashicorp-vault"[0].credentials.backends.generic[0]')"
