5e1c6686ffb441fadace9e3563fcd65e0047627d
Joshua Trzebiatowski <joshua.trzebiatowski@kohls.com>
2022-05-09 16:30:22 -0500

    vault fix

--- run.sh
+++ run.sh
@@ -7,20 +7,20 @@ wget -O jq "$JQ_BIN_URL"
 wget -O vault.zip "$VAULT_BIN_URL"
 unzip vault.zip
 
-export VAULT_ADDR="$(echo "$VCAP_SERVICES" | ./jq '."hashicorp-vault"[0].credentials.address')"
+VAULT_ADDR="$(echo "$VCAP_SERVICES" | ./jq '."hashicorp-vault"[0].credentials.address')"
 VAULT_TOKEN="$(echo "$VCAP_SERVICES" | ./jq '."hashicorp-vault"[0].credentials.auth.token')"
 SERVICE_ENGINE="$(echo "$VCAP_SERVICES" | ./jq '."hashicorp-vault"[0].credentials.backends.generic[0]')"
 APPLICATION_ENGINE="$(echo "$VCAP_SERVICES" | ./jq '."hashicorp-vault"[0].credentials.backends_shared.application')"
 SPACE_ENGINE="$(echo "$VCAP_SERVICES" | ./jq '."hashicorp-vault"[0].credentials.backends_shared.space')"
 ORGANIZATION_ENGINE="$(echo "$VCAP_SERVICES" | ./jq '."hashicorp-vault"[0].credentials.backends_shared.organization')"
-./vault login "$VAULT_TOKEN"
+./vault login -address="$VAULT_ADDR" "$VAULT_TOKEN"
 
 while true; do
   ./vault token renew
-  echo "SERVICE:      $(./vault read "${SERVICE_ENGINE}/secret" -field=SECRET_TYPE)"
-  echo "APPLICATION:  $(./vault read "${APPLICATION_ENGINE}/secret" -field=SECRET_TYPE)"
-  echo "SPACE:        $(./vault read "${SPACE_ENGINE}/secret" -field=SECRET_TYPE)"
-  echo "ORGANIZATION: $(./vault read "${ORGANIZATION_ENGINE}/secret" -field=SECRET_TYPE)"
+  echo "SERVICE:      $(./vault read -address="$VAULT_ADDR" "${SERVICE_ENGINE}/secret" -field=SECRET_TYPE)"
+  echo "APPLICATION:  $(./vault read -address="$VAULT_ADDR" "${APPLICATION_ENGINE}/secret" -field=SECRET_TYPE)"
+  echo "SPACE:        $(./vault read -address="$VAULT_ADDR" "${SPACE_ENGINE}/secret" -field=SECRET_TYPE)"
+  echo "ORGANIZATION: $(./vault read -address="$VAULT_ADDR" "${ORGANIZATION_ENGINE}/secret" -field=SECRET_TYPE)"
   echo ''
   sleep 10
 done
